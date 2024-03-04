5df595b07964936f5427cd90dce97d220c836b89
John James <ospina.john@correounivalle.edu.co>
2024-03-04 18:56:12 -0500

    Agregado componente UserItem y dependencia @radix-ui/react-avatar

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,6 +5,7 @@ import { ChevronsLeft, MenuIcon } from 'lucide-react';
 import { usePathname } from 'next/navigation';
 import { ElementRef, use, useEffect, useRef, useState } from 'react';
 import { useMediaQuery } from 'usehooks-ts';
+import { UserItem } from './user-item';
 
 export const Navigation = () => {
 	const pathanme = usePathname(); // Esto es para saber la ruta actual
@@ -112,7 +113,7 @@ export const Navigation = () => {
 					<ChevronsLeft className='w-6 h-6' />
 				</div>
 				<div>
-					<p>Action items</p>
+					<UserItem />
 				</div>
 				<div className='mt-4'>
 					<p>Documents</p>
---
+++ app/(main)/_components/user-item.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export const UserItem = () => {
+	return (
+		<div>
+			<h1>User Item</h1>
+		</div>
+	);
+};
+
+
