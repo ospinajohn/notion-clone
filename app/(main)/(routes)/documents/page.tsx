46e0df5332c7c70a42c9258f9994b955027f0a39
John James <ospina.john@correounivalle.edu.co>
2024-03-04 18:48:59 -0500

    Añadida página de documentos protegida con imagen y botón de creación de notas

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,36 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { useUser } from '@clerk/clerk-react';
+import { PlusCircle } from 'lucide-react';
+import Image from 'next/image';
+
 const DocumentsPage = () => {
+	const { user } = useUser();
+
 	return (
-		<div>
-			<h1>This is a protected Documents page.</h1>
+		<div className='h-full flex flex-col items-center justify-center space-y-4'>
+			<Image
+				src='/Empty.png'
+				height={300}
+				width={300}
+				alt='Empty'
+				className='dark:hidden'
+			/>
+			<Image
+				src='/Empty-dark.png'
+				height={300}
+				width={300}
+				alt='Empty'
+				className='hidden dark:block'
+			/>
+			<h2 className='text-lg font-medium'>
+				Welcome to {user?.firstName}&apos;s Jotion
+			</h2>
+			<Button>
+				<PlusCircle className='w-4 h-4 mr-2' />
+				Create a new note
+			</Button>
 		</div>
 	);
 };
