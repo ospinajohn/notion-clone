0d0fd7ddb817cafd74c7efcb754fb678816c3f0c
John James <ospina.john@correounivalle.edu.co>
2024-03-05 19:50:58 -0500

    Añadir función para crear un nuevo documento

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,12 +1,28 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
+import { api } from '@/convex/_generated/api';
 import { useUser } from '@clerk/clerk-react';
+import { useMutation } from 'convex/react';
 import { PlusCircle } from 'lucide-react';
 import Image from 'next/image';
+import { toast } from 'sonner';
 
 const DocumentsPage = () => {
 	const { user } = useUser();
+	const create = useMutation(api.documents.create);
+
+	const onCreate = async () => {
+		const document = await create({
+			title: 'Untitled',
+		});
+
+		if (document) {
+			toast.success('Document created');
+		} else {
+			toast.error('Failed to create document');
+		}
+	};
 
 	return (
 		<div className='h-full flex flex-col items-center justify-center space-y-4'>
@@ -27,7 +43,7 @@ const DocumentsPage = () => {
 			<h2 className='text-lg font-medium'>
 				Welcome to {user?.firstName}&apos;s Jotion
 			</h2>
-			<Button>
+			<Button onClick={onCreate}>
 				<PlusCircle className='w-4 h-4 mr-2' />
 				Create a new note
 			</Button>
--- convex/documents.ts
+++ convex/documents.ts
@@ -3,6 +3,7 @@ import { v } from 'convex/values';
 import { mutation, query } from './_generated/server';
 import { Doc, Id } from './_generated/dataModel';
 
+// This is a function that creates a new document in the database.
 export const create = mutation({
 	args: {
 		title: v.string(),
